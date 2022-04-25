f8c05b5eb743421badae6fb19cd5d3d414e6eee4
Adrian Salceanu <adrian.salceanu@gmail.com>
2022-04-25 19:52:54 +0200

    initial commit

--- src/GenieDeploy.jl
+++ src/GenieDeploy.jl
@@ -1,5 +1,15 @@
 module GenieDeploy
 
-# Write your package code here.
+function run(command::Cmd)
+  try Base.run(command)
+
+  catch ex
+    if isa(ex, Base.IOError)
+      @error "Can not find $(command.exec[1]). Please make sure that $(command.exec[1]) is installed and accessible."
+    end
+
+    rethrow(ex)
+  end
+end
 
 end
